f5842692a5a95e516ba13b5a67d167300902b80a
Sabrina Paola <sabripaola1808@gmail.com>
2019-01-22 15:40:37 +0100

    esercizio terminato

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+    $name=Sabrina;
+    $surname=Paola;
+
+    echo "Hi $name $surname!";
+
+
+     ?>
+
+  </body>
+</html>
